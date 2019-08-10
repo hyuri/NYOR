74d5383e05ccbd16b55701e60814a2bf882f8da1
hyuri <hyuri.pimentel@gmail.com>
2019-08-10 02:52:18 -0300

    Updated with example project.

--- NYOR.py
+++ NYOR.py
@@ -4,12 +4,12 @@ from pathlib import Path, PurePath
 from NYOR_Foundation import *
 
 # -------------------------------------------------------------------------------
-# NYOR / Nyor / N.Y.O.R. | Nuke Your Old Renders
+# NYOR / N.Y.O.R. | Nuke Your Old Renders
 # Deletes every render(and .tmp files) in your NukeStudio project except the one for the highest version of each comp.
 # -------------------------------------------------------------------------------
 # Tested:
-# Python 3.6
-# Nuke 11.1v4
+# Python 3.6 (requires at least 3.6)
+# NukeStudio 11.1v4
 # -------------------------------------------------------------------------------
 # TODO:
 # - Add logging
@@ -17,39 +17,50 @@ from NYOR_Foundation import *
 
 # -------------------------------------------------------------------------------
 # Project Settings
-# project_name: Name should be the same as project's folder name
-# NS_folder: NukeStudio project root folder
+# project_name: Name should be the same as project's folder name in the file system
+# NS_folder: NukeStudio project root folder in the file system
 
-film_projects = ProjectsLibrary("Filmes", "D:/Projetos/Ativos/Filmes")
+# (i) Below is an example project. Change the paths and names accordingly.
 
-project_folder = "2018 Neptunea"
+# We create a Projects Library
+film_projects = ProjectsLibrary("Filmes", "D:/Projects/Active/Films")
+
+# Indicate which project in that library we want to open
+project_folder = "2018MyFilmProject"
+
+# And, finally, indicate the folder where your NukeStudio project lives in (inside project_folder)
 NS_folder = "VFX/NUKE"
 
-# -------------------------------------------------------------------------------
-# Operations
+# To make it clearer: "2018MyFilmProject" is a folder that lives inside the "D:/Projects/Active/Films" folder
+# So, in this case, the full path where the NukeStudio project lives in would be:
+# "D:/Projects/Active/Films/2018MyFilmProject/VFX/NUKE"
+# The NukeStudio project file name itself doesn't matter, as long as you only have one inside that folder.
 
-aum_vfx = Project(film_projects, project_folder, NS_folder)
+# -------------------------------------------------------------------------------
+# UI
+
+myfilm_vfx = Project(film_projects, project_folder, NS_folder)
 
 options = {
 			1: {
 				"title": "Print Hierarchy",
 				"uid": "print_hierarchy",
-				"command": aum_vfx.print_hierarchy
+				"command": myfilm_vfx.print_hierarchy
 			},
 			2: {
 				"title": "Delete Renders of Old Versions",
 				"uid": "del_all_renders_old_versions",
-				"command": aum_vfx.delete_renders_of_older_versions # send_to_trash is set to "True" by default.
+				"command": myfilm_vfx.delete_renders_of_older_versions # send_to_trash is set to "True" by default.
 			},
 			3: {
 				"title": "Delete Tmp Render Files",
 				"uid": "del_all_render_tmp_files",
-				"command": aum_vfx.delete_render_tmp_files # send_to_trash is set to "True" by default.
+				"command": myfilm_vfx.delete_render_tmp_files # send_to_trash is set to "True" by default.
 			},
 			4: {
 				"title": "Delete Renders of Latest Version",
 				"uid": "del_all_renders_latest_version",
-				"command": aum_vfx.delete_renders_of_latest_version # send_to_trash is set to "True" by default.
+				"command": myfilm_vfx.delete_renders_of_latest_version # send_to_trash is set to "True" by default.
 			},
 			5: {
 				"title": "Exit.",
